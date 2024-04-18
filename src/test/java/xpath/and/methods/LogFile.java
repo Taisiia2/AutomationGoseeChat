395c32266c2a55de01e4808e63dca81eba1114fd
Дмитрий <krasnyydmitriy@gmail.com>
2024-04-18 17:10:28 +0300

    implementation manual log file

--- src/test/java/xpath/and/methods/LogFile.java
+++ src/test/java/xpath/and/methods/LogFile.java
@@ -1,14 +1,25 @@
 package xpath.and.methods;
 
 import java.io.*;
+import java.util.Date;
 
 public class LogFile {
-    public static void LogIntoFile(String MsgToFile) throws IOException {
+    public static void LogIntoFile(String MsgToFileTrue,String MsgToFileFalse, boolean res) throws IOException {
+        Date date = new Date();
         File f1 = new File("log.txt");
         FileWriter fw = new FileWriter(f1, true);  // Залишити коментар
         PrintWriter pw = new PrintWriter(fw);
-        pw.println(MsgToFile);
+        if (res == true){
+            pw.println(date +" "+MsgToFileTrue);
+        }else {
+            pw.println(date +" "+MsgToFileFalse);
+        }
         pw.flush();
         pw.close();
+    }
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
     }
 }
